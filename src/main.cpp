74cc2c558ad95c6b9f95ef491ec068d1b5749733
Matthieu Gallien <matthieu_gallien@yahoo.fr>
2018-09-07 17:16:20 +0200

    bump version in preparation for the next 0.4 beta

--- src/main.cpp
+++ src/main.cpp
@@ -87,7 +87,7 @@ int main(int argc, char *argv[])
 
     KAboutData aboutData( QStringLiteral("elisa"),
                           i18n("Elisa"),
-                          QStringLiteral("0.3.0"),
+                          QStringLiteral("0.3.80"),
                           i18n("A Simple Music Player written with KDE Frameworks"),
                           KAboutLicense::LGPL_V3,
                           i18n("(c) 2015-2018, Elisa contributors"));

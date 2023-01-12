KDE/elisa

commit be5d35bf932235f010bff61c021ec17fe6a0ac69

Make button menus behave in a more native way
Inspired by the change in
https://invent.kde.org/plasma/systemsettings/-/merge_requests/187.

src/qml/MediaPlayerControl.qml
@@ -201,9 +201,13 @@ BasePlayerControl {
             icon.name: "open-menu-symbolic"
 
             down: pressed || applicationMenu.visible
+            Accessible.role: Accessible.ButtonMenu
+
             onPressed: {
                 if (!applicationMenu.visible) {
                     applicationMenu.open()
+                } else {
+                    applicationMenu.dismiss()
                 }
             }
 

src/qml/NavigationActionBar.qml
@@ -60,23 +60,14 @@ Item {
             Kirigami.Theme.colorSet: Kirigami.Settings.isMobile ? Kirigami.Theme.Complementary : Kirigami.Theme.Window
             Kirigami.Theme.inherit: false
             display: AbstractButton.TextOnly
-
-            checkable: true
-            checked: sortMenu.visible
-
-            onClicked: {
-                if (sortMenu.visible) {
-                    sortMenu.dismiss()
-                } else {
-                    sortMenu.sortOrder = navigationBar.sortOrder
-                    sortMenu.popup(sortMenuButton, sortMenuButton.x, sortMenuButton.y + sortMenuButton.height)
-                }
-            }
+            down: sortMenu.visible || pressed
+            Accessible.role: Accessible.ButtonMenu
 
             // Custom content item for now to replicate the look of a button
             // that opens a dropdown menu
             // TODO: Port more fully to Kirigami so we get this for free
             contentItem: RowLayout {
+                id: internalLayout
                 spacing: Kirigami.Units.smallSpacing
 
                 Kirigami.Icon {
@@ -95,13 +86,17 @@ Item {
                     }
                 }
 
-                Kirigami.Icon {
-                    Layout.leftMargin: Kirigami.Units.smallSpacing
-                    Layout.rightMargin: Kirigami.Units.largeSpacing
-                    implicitWidth: Kirigami.Units.iconSizes.small
-                    implicitHeight: Kirigami.Units.iconSizes.small
-                    source: 'arrow-down'
-                    isMask: true
+                Item {
+                    implicitWidth: Kirigami.Units.iconSizes.small + internalLayout.spacing
+                }
+            }
+
+            onPressed: {
+                if (sortMenu.visible) {
+                    sortMenu.dismiss()
+                } else {
+                    sortMenu.sortOrder = navigationBar.sortOrder
+                    sortMenu.popup(sortMenuButton, sortMenuButton.x, sortMenuButton.y + sortMenuButton.height)
                 }
             }
         }

commit 3b32d3fd6b8ec4a6ec032939edf76474e31e0606

Make menu buttons open on return/enter again
Partially broken with be5d35bf932235f010bff61c021ec17fe6a0ac69.

BUG: 453591
FIXED-IN: 23.04

src/qml/MediaPlayerControl.qml
@@ -245,19 +245,23 @@ BasePlayerControl {
         FlatButtonWithToolTip {
             id: menuButton
 
-            text: i18nc("@action:button", "Application Menu")
-            icon.name: "open-menu-symbolic"
-
-            down: pressed || applicationMenu.visible
-            Accessible.role: Accessible.ButtonMenu
-
-            onPressed: {
+            function openMenu() {
                 if (!applicationMenu.visible) {
                     applicationMenu.open()
                 } else {
                     applicationMenu.dismiss()
                 }
             }
+
+            text: i18nc("@action:button", "Application Menu")
+            icon.name: "open-menu-symbolic"
+
+            down: pressed || applicationMenu.visible
+            Accessible.role: Accessible.ButtonMenu
+
+            onPressed: openMenu()
+            // Need this too because the base control sends onClicked for return/enter
+            onClicked: openMenu()
 
             ApplicationMenu {
                 id: applicationMenu

src/qml/NavigationActionBar.qml
@@ -57,6 +57,16 @@ Item {
         id: sortMenuComponent
         FlatButtonWithToolTip {
             id: sortMenuButton
+
+            function openMenu() {
+                if (sortMenu.visible) {
+                    sortMenu.dismiss()
+                } else {
+                    sortMenu.sortOrder = navigationBar.sortOrder
+                    sortMenu.popup(sortMenuButton, sortMenuButton.x, sortMenuButton.y + sortMenuButton.height)
+                }
+            }
+
             Kirigami.Theme.colorSet: Kirigami.Settings.isMobile ? Kirigami.Theme.Complementary : Kirigami.Theme.Window
             Kirigami.Theme.inherit: false
             display: AbstractButton.TextOnly
@@ -91,14 +101,9 @@ Item {
                 }
             }
 
-            onPressed: {
-                if (sortMenu.visible) {
-                    sortMenu.dismiss()
-                } else {
-                    sortMenu.sortOrder = navigationBar.sortOrder
-                    sortMenu.popup(sortMenuButton, sortMenuButton.x, sortMenuButton.y + sortMenuButton.height)
-                }
-            }
+            onPressed: openMenu()
+            // Need this too because the base control sends onClicked for return/enter
+            onClicked: openMenu()
         }
     }
 
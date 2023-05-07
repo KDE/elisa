30d10b6ca72a16e7759473b6683505d5bf376ecc
Matthieu Gallien <matthieu_gallien@yahoo.fr>
2023-05-07 11:06:09 +0200

    remove qml unit tests: deemed not useful

--- autotests/elisaqmltests.cpp
+++
@@ -1,9 +0,0 @@
-/*
-   SPDX-FileCopyrightText: 2018 (c) Matthieu Gallien <matthieu_gallien@yahoo.fr>
-
-   SPDX-License-Identifier: LGPL-3.0-or-later
- */
-
-#include <QtQuickTest>
-
-QUICK_TEST_MAIN(ElisaQmlTests)

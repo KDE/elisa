KDE/elisa

commit e3a2ec6afb67e2a1265d4426c1cddbd5d7f75a27

Refactor setting initial files view path
Now set it as a Q_PROPERTY so the initial path can be updated during runtime,
instead of being set at startup

src/viewmanager.cpp
@@ -93,6 +93,8 @@ public:
 
     int mInitialIndex = -1;
 
+    QString mInitialFilesViewPath = QDir::rootPath();
+
     QList<ViewParameters> mViewParametersStack = (mViewsListData ? QList<ViewParameters>{mViewsListData->viewParameters(0)} : QList<ViewParameters>{});
 };
 
@@ -115,6 +117,11 @@ int ViewManager::initialIndex() const
 ViewsListData *ViewManager::viewsData() const
 {
     return d->mViewsListData;
+}
+
+QString ViewManager::initialFilesViewPath() const
+{
+    return d->mInitialFilesViewPath;
 }
 
 ViewManager::~ViewManager() = default;
@@ -340,6 +347,10 @@ void ViewManager::openViewFromData(const ViewParameters &viewParamaters)
 void ViewManager::applyFilter(ViewParameters &nextViewParameters,
                               QString title, const ViewParameters &lastView) const
 {
+    if (nextViewParameters.mDataType == ElisaUtils::FileName) { // Folder view
+        nextViewParameters.mDataFilter[DataTypes::FilePathRole] = QUrl::fromLocalFile(d->mInitialFilesViewPath);
+    }
+
     switch (nextViewParameters.mFilterType)
     {
     case ElisaUtils::NoFilter:
@@ -520,6 +531,16 @@ void ViewManager::setInitialIndex(int newIndex)
     if (d->mViewsListData && d->mViewIndex >= 0 && d->mViewIndex < d->mViewsListData->count() && d->mViewsListData->isFullyInitialized()) {
         openView(d->mViewIndex);
     }
+}
+
+void ViewManager::setInitialFilesViewPath(const QString &initialPath)
+{
+    if (d->mInitialFilesViewPath == initialPath) {
+        return;
+    }
+
+    d->mInitialFilesViewPath = initialPath;
+    Q_EMIT initialFilesViewPathChanged();
 }
 
 void ViewManager::openInitialView()

src/elisaapplication.cpp
@@ -359,6 +359,7 @@ void ElisaApplication::configChanged()
     Q_EMIT useFavoriteStyleRatingsChanged();
     Q_EMIT embeddedViewChanged();
     Q_EMIT initialViewIndexChanged();
+    Q_EMIT initialFilesViewPathChanged();
 }
 
 DataTypes::EntryDataList ElisaApplication::checkFileListAndMakeAbsolute(const DataTypes::EntryDataList &filesList,
@@ -669,5 +670,11 @@ int ElisaApplication::initialViewIndex() const
 
     return result;
 }
+
+QString ElisaApplication::initialFilesViewPath() const
+{
+    return Elisa::ElisaConfiguration::initialFilesViewPath();
+}
+
 
 #include "moc_elisaapplication.cpp"

src/elisaapplication.h
@@ -97,6 +97,10 @@ class ELISALIB_EXPORT ElisaApplication : public QObject
                READ initialViewIndex
                NOTIFY initialViewIndexChanged)
 
+    Q_PROPERTY(QString initialFilesViewPath
+               READ initialFilesViewPath
+               NOTIFY initialFilesViewPathChanged)
+
 public:
     explicit ElisaApplication(QObject *parent = nullptr);
 
@@ -139,6 +143,8 @@ public:
 
     int initialViewIndex() const;
 
+    [[nodiscard]] QString initialFilesViewPath() const;
+
 Q_SIGNALS:
 
     void musicManagerChanged();
@@ -174,6 +180,8 @@ Q_SIGNALS:
     void embeddedViewChanged();
 
     void initialViewIndexChanged();
+
+    void initialFilesViewPathChanged();
 
 public Q_SLOTS:
 

src/qml/ContentView.qml
@@ -59,6 +59,7 @@ RowLayout {
         id: viewManager
 
         viewsData: viewsData
+        initialFilesViewPath: ElisaApplication.initialFilesViewPath
 
         onOpenGridView: configurationData => {
             openViewCommon(dataGridView, {

src/viewmanager.h
@@ -38,6 +38,11 @@ class ELISALIB_EXPORT ViewManager : public QObject
                WRITE setInitialIndex
                NOTIFY initialIndexChanged)
 
+    Q_PROPERTY(QString initialFilesViewPath
+               READ initialFilesViewPath
+               WRITE setInitialFilesViewPath
+               NOTIFY initialFilesViewPathChanged)
+
     Q_PROPERTY(ViewsListData* viewsData READ viewsData WRITE setViewsData NOTIFY viewsDataChanged)
 
 public:
@@ -102,6 +107,8 @@ public:
 
     [[nodiscard]] int initialIndex() const;
 
+    [[nodiscard]] QString initialFilesViewPath() const;
+
     [[nodiscard]] ViewsListData* viewsData() const;
 
 Q_SIGNALS:
@@ -119,6 +126,8 @@ Q_SIGNALS:
     void initialIndexChanged();
 
     void viewsDataChanged();
+
+    void initialFilesViewPathChanged();
 
 public Q_SLOTS:
 
@@ -141,6 +150,8 @@ public Q_SLOTS:
     void sortRoleChanged(int sortRole);
 
     void setInitialIndex(int newIndex);
+
+    void setInitialFilesViewPath(const QString &initialPath);
 
 private Q_SLOTS:
 

src/viewslistdata.cpp
@@ -6,10 +6,8 @@
 
 #include "viewslistdata.h"
 
-#include "elisaapplication.h"
 #include "modeldataloader.h"
 #include "databaseinterface.h"
-#include "elisa_settings.h"
 #include "musiclistenersmanager.h"
 
 #include "viewsLogging.h"
@@ -23,11 +21,6 @@
 class ViewsListDataPrivate
 {
 public:
-    explicit ViewsListDataPrivate(const QString &initialFilesViewPath)
-        : mInitialFilesViewPath{initialFilesViewPath} {}
-
-    QString mInitialFilesViewPath;
-
     QList<ViewParameters> mViewsParameters = {{{i18nc("@title:window Title of the view of the playlist", "Now Playing")},
                                                QUrl{QStringLiteral("image://icon/view-media-lyrics")},
                                                ViewManager::ContextView},
@@ -131,7 +124,7 @@ public:
                                                QUrl{QStringLiteral("image://icon/document-open-folder")},
                                                ViewManager::DelegateWithoutSecondaryText,
                                                ViewManager::ViewHideRating,
-                                               QUrl::fromLocalFile(mInitialFilesViewPath)
+                                               QUrl::fromLocalFile(QDir::rootPath())
                                               },
                                               {{i18nc("@title:window Title of the file radios browser view", "Radio Stations")},
                                                QUrl{QStringLiteral("image://icon/radio")},
@@ -165,15 +158,10 @@ public:
     bool mIsFullyInitialized = true;
 };
 
-ViewsListData::ViewsListData(QObject *parent) : QObject(parent) {
-    // Need to instantiate a config object to read the user's preferred initial Files view path
-    auto configurationFileName = QStandardPaths::writableLocation(QStandardPaths::ConfigLocation);
-    configurationFileName += QStringLiteral("/elisarc");
-    Elisa::ElisaConfiguration::instance(configurationFileName);
-    Elisa::ElisaConfiguration::self()->load();
-
-    d = std::make_unique<ViewsListDataPrivate>(Elisa::ElisaConfiguration::initialFilesViewPath());
-
+ViewsListData::ViewsListData(QObject *parent)
+    : QObject(parent)
+    , d(std::make_unique<ViewsListDataPrivate>())
+{
     d->mDataLoader = new ModelDataLoader;
     connect(this, &ViewsListData::destroyed, d->mDataLoader, &ModelDataLoader::deleteLater);
 }

commit 5189a1ee99a5afb2f1e6c61b6a58a388a776f1f2

Only set the folder view's path when opening the view from the sidebar
So that subsequent child views don't reset to the default initial path.

I accidentally broke this in e3a2ec6a

src/viewmanager.cpp
@@ -148,6 +148,9 @@ void ViewManager::openView(int viewIndex)
     Q_EMIT viewIndexChanged();
 
     applyFilter(viewParameters, viewParameters.mMainTitle, viewParameters);
+    if (viewParameters.mDataType == ElisaUtils::FileName) { // Folder view
+        viewParameters.mDataFilter[DataTypes::FilePathRole] = QUrl::fromLocalFile(d->mInitialFilesViewPath);
+    }
     openViewFromData(viewParameters);
 }
 
@@ -347,10 +350,6 @@ void ViewManager::openViewFromData(const ViewParameters &viewParamaters)
 void ViewManager::applyFilter(ViewParameters &nextViewParameters,
                               QString title, const ViewParameters &lastView) const
 {
-    if (nextViewParameters.mDataType == ElisaUtils::FileName) { // Folder view
-        nextViewParameters.mDataFilter[DataTypes::FilePathRole] = QUrl::fromLocalFile(d->mInitialFilesViewPath);
-    }
-
     switch (nextViewParameters.mFilterType)
     {
     case ElisaUtils::NoFilter:
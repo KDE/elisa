c699d69e351230b19b9bd7c90a4d304b3d580a26
Jack Hill <jackhill3103@gmail.com>
2024-01-28 10:47:36 +0000

    Android: fix missing icons

--- src/elisaapplication.cpp
+++ src/elisaapplication.cpp
@@ -387,12 +387,6 @@ void ElisaApplication::initialize()
     initializeModels();
     initializePlayer();
 
-#ifdef Q_OS_ANDROID
-    QIcon::setThemeName(QStringLiteral("Breeze"));
-    QIcon::setFallbackSearchPaths({QIcon::themeSearchPaths().first() + QStringLiteral("/icons")});
-    QIcon::setThemeSearchPaths({QIcon::themeSearchPaths().first()});
-#endif
-
     Q_EMIT initializationDone();
 }
 

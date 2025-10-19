8351c6bc30de4bbb3eab732745c0222a2170ce9d
l10n daemon script <scripty@kde.org>
2025-10-19 03:16:02 +0000

    SVN_SILENT made messages (.desktop file) - always resolve ours
    In case of conflict in i18n, keep the version of the branch "ours"
    To resolve a particular conflict, "git checkout --ours path/to/file.desktop"

--- org.kde.elisa.desktop.cmake
+++ org.kde.elisa.desktop.cmake
@@ -26,6 +26,7 @@ Comment[nl]=Speel lokaal muziek en luister naar online radio
 Comment[nn]=Spel av lokal musikk og lytt på nettradio
 Comment[pl]=Odtwarzaj muzykę lokalnie i słucha radia z sieci
 Comment[pt_BR]=Reproduza músicas locais e ouça rádios online
+Comment[ro]=Redă muzică locală și ascultă stații radio prin Internet
 Comment[ru]=Воспроизведение хранящейся локально музыки и прослушивание интернет-радиостанций
 Comment[sa]=स्थानीयसङ्गीतं वादयन्तु, ऑनलाइन रेडियो शृणुत च
 Comment[sl]=Predvaja lokalno glasbo in posluša spletni radio
